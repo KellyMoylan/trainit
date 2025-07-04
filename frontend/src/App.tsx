5d09fc2654ab0c5763589dd10bdbb61798d08b97
Kelly Moylan <kelly.moylan@gmail.com>
2025-07-04 15:50:56 -0400

    typescript errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect} from 'react'
 import { Routes, Route, Link, useNavigate } from 'react-router-dom'
 import './App.css'
 import React from 'react'
